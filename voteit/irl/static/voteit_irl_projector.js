a7ea2b211e401df0d905ff0e0c39ea5acfaacb2c
robinharms <robin@betahaus.net>
2013-04-27 17:21:51 +0200

    Projector view fixes. Should work with new design now. might require some modifications and some cleanup

--- voteit/irl/static/voteit_irl_projector.js
+++ voteit/irl/static/voteit_irl_projector.js
@@ -2,30 +2,18 @@
 $('.dropdown_menu .menu_header').live('hover', function(event) { dropdown_menus(event, this, '') });
 
 $('a.activate').live('click', function(event) {
-    /* stop form from submitting normally 
-    IE might throw an error calling preventDefault(), so use a try/catch block. */
     try { event.preventDefault(); } catch(e) {}
-    
     $(this).parents('li').appendTo($('#active > ul'));
 });
 $('a.inactivate').live('click', function(event) {
-    /* stop form from submitting normally 
-    IE might throw an error calling preventDefault(), so use a try/catch block. */
     try { event.preventDefault(); } catch(e) {}
-    
     $(this).parents('li').appendTo($('#inactive > ul'));
 });
-
-$('.state a').live('click', function(event) {
-    /* stop form from submitting normally 
-    IE might throw an error calling preventDefault(), so use a try/catch block. */
+$('.states a').live('click', function(event) {
     try { event.preventDefault(); } catch(e) {}
-    
     var url = $(this).attr("href");
     var li = $(this).parents('li');
     $.get(url, function(data) {
         li.replaceWith(data)
     });
 });
-
-
