034cc254828be9661315e60225c109eb9da1da69
robinharms <robin@betahaus.net>
2013-04-27 13:18:35 +0200

    Fixed projector view so it starts, but without any design.

--- voteit/irl/static/voteit_irl_projector.js
+++ voteit/irl/static/voteit_irl_projector.js
@@ -1,4 +1,5 @@
-//$('#projector .dropdown_menu').live('hover', display_qtip_menu);
+// Bind to menu function
+$('.dropdown_menu .menu_header').live('hover', function(event) { dropdown_menus(event, this, '') });
 
 $('a.activate').live('click', function(event) {
     /* stop form from submitting normally 
@@ -27,38 +28,4 @@ $('.state a').live('click', function(event) {
     });
 });
 
-$('#agenda-items-menu.dropdown_menu').live('hover', display_meeting_menu);
-function display_meeting_menu(event) {
-    /* stop form from submitting normally 
-    IE might throw an error calling preventDefault(), so use a try/catch block. */
-    try { event.preventDefault(); } catch(e) {}
-    event.preventDefault(); 
 
-    $(this).qtip({
-        overwrite: false, // Make sure the tooltip won't be overridden once created
-        content: { 
-            text: $(this).find('.menu_body'), // The text to use whilst the AJAX request is loading
-        },
-        show: {
-            event: event.type, // Use the same show event as the one that triggered the event handler
-            ready: true, // Show the tooltip as soon as it's bound, vital so it shows up the first time you hover!
-            effect: false,
-        },
-        hide: {
-            event: "mouseleave",
-            fixed: true,
-            effect: false,
-        },
-        position: {
-            viewport: $(window),
-            at: "right bottom",
-            my: "right top",
-            adjust: {
-                method: 'flip',
-            }
-        },
-        style: {
-            classes: "qtip_menu agenda-items-menu-body",
-        },
-    }, event);
-}
