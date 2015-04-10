4939fb511080114d385a33f871404b4f4dd1aa29
robinharms <robin@betahaus.net>
2015-04-10 15:14:06 +0200

    Typo

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 
 requires = (
-    'Arche'
+    'Arche',
     'fanstatic',
     'pyramid',
     'six',
