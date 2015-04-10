c8176a163e6826ac1d8d591cacc2b4a5379f2754
robinharms <robin@betahaus.net>
2015-04-10 17:05:49 +0200

    Check for participant number before setting voter

--- voteit/irl/plugins/present_users_voters.py
+++ voteit/irl/plugins/present_users_voters.py
@@ -1,21 +1,30 @@
 from __future__ import unicode_literals
 
 from voteit.core.models.interfaces import IMeeting
+from pyramid.threadlocal import get_current_request
 
 from voteit.irl import _
 from voteit.irl.models.elegible_voters_method import ElegibleVotersMethod
 from voteit.irl.models.interfaces import IMeetingPresence
+from voteit.irl.models.interfaces import IParticipantNumbers
 
 
 class MakePresentUsersVoters(ElegibleVotersMethod):
-    name = 'present_users_voters'
-    title = _("Set present users as voters")
-    description = _("Will remove voting permission for anyone not set as present.")
+    name = 'present_with_pn_voters'
+    title = _("Present with participant number")
+    description = _("Will remove voting permission for anyone not set as present. "
+                    "Users without a participant number will be ignored.")
 
     def get_voters(self, request = None, **kw):
+        if request is None:
+            request = get_current_request()
         meeting_presence = request.registry.getAdapter(self.context, IMeetingPresence)
-        return frozenset(meeting_presence.present_userids)
-
+        participant_numbers = request.registry.getAdapter(self.context, IParticipantNumbers)
+        results = set()
+        for userid in meeting_presence.present_userids:
+            if userid in participant_numbers.userid_to_number:
+                results.add(userid)
+        return frozenset(results)
 
 def includeme(config):
     config.registry.registerAdapter(MakePresentUsersVoters, name = MakePresentUsersVoters.name)
