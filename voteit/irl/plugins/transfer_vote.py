4d78f37b6e0d13866c8690cc0cde0a57cca0f5c4
Robin Harms Oredsson <robin@betahaus.net>
2017-05-27 10:44:16 +0200

    Possibility to transfer vote to a participant number too

--- voteit/irl/plugins/transfer_vote.py
+++ voteit/irl/plugins/transfer_vote.py
@@ -1,6 +1,7 @@
 import colander
 from arche.views.base import DefaultEditForm
 from betahaus.viewcomponent import view_action
+from pyramid.httpexceptions import HTTPBadRequest
 from pyramid.httpexceptions import HTTPForbidden
 from pyramid.httpexceptions import HTTPFound
 from pyramid.traversal import resource_path
@@ -8,7 +9,9 @@ from repoze.catalog.query import Eq
 from voteit.core import security
 from voteit.core.models.interfaces import IMeeting
 from voteit.core.schemas.common import deferred_autocompleting_userid_widget
+
 from voteit.irl import _
+from voteit.irl.models.interfaces import IParticipantNumbers
 
 
 @view_action('participants_menu', 'transfer_vote', title=_("Transfer vote"))
@@ -17,6 +20,17 @@ def generic_menu_link(context, request, va, **kw):
     if security.ROLE_VOTER in request.meeting.local_roles.get(request.authenticated_userid, ()):
         url = request.resource_url(request.meeting, 'transfer_vote')
         return """<li><a href="%s">%s</a></li>""" % (url, request.localizer.translate(va.title))
+
+
+def _convert_to_userid(context, value):
+    """ Maybe convert a string with participant number to userid? """
+    try:
+        value = int(value)
+    except ValueError:
+        # Should be a regular text string
+        return value
+    pn = IParticipantNumbers(context)
+    return pn.number_to_userid.get(value, None)
 
 
 @colander.deferred
@@ -28,6 +42,9 @@ class ReceivingUserIDValidator(object):
             self.context), "context must be a meeting object, got %r" % self.context
 
     def __call__(self, node, value):
+        value = _convert_to_userid(self.context, value)
+        if not value:
+            raise colander.Invalid(node, _("No user has that participant number"))
         if value not in security.find_authorized_userids(self.context, [security.VIEW]):
             raise colander.Invalid(node, _("${userid} doesn't exist in this meeting.",
                                            mapping={'userid': value}))
@@ -39,7 +56,7 @@ class ReceivingUserIDValidator(object):
 class TransferVoteSchema(colander.Schema):
     to_userid = colander.SchemaNode(
         colander.String(),
-        title=_("To userid"),
+        title=_("To userid or participant number"),
         description=_("Must be someone who isn't a voter right now."),
         validator=ReceivingUserIDValidator,
         widget=deferred_autocompleting_userid_widget
@@ -63,6 +80,9 @@ class TransferVoteForm(DefaultEditForm):
 
     def save_success(self, appstruct):
         userid = appstruct.pop('to_userid')
+        userid = _convert_to_userid(self.context, userid)
+        if userid is None:
+            raise HTTPBadRequest("No userid found")
         self.context.del_groups(self.request.authenticated_userid, [security.ROLE_VOTER])
         self.context.add_groups(userid, [security.ROLE_VOTER])
         self.flash_messages.add(_("Vote transfered to ${userid}", mapping={'userid': userid}))
