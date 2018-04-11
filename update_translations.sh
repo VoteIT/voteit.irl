2581c90b3737ec059332d541426b9a6c8151c434
Robin Harms Oredsson <robin@betahaus.net>
2018-04-11 10:46:44 +0200

    Fix of presence check

--- update_translations.sh
+++
@@ -1,9 +0,0 @@
- #!/bin/bash
- #You need lingua and gettext installed to run this
- 
- echo "Updating voteit.irl.pot"
- pot-create -d voteit.irl -o voteit/irl/locale/voteit.irl.pot .
- echo "Merging Swedish localisation"
- msgmerge --update voteit/irl/locale/sv/LC_MESSAGES/voteit.irl.po voteit/irl/locale/voteit.irl.pot
- echo "Updated locale files"
- 
