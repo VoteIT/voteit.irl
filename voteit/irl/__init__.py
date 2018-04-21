94f1c8d5efd242894ffac6cf7915cefd3d907edf
Robin Harms Oredsson <robin@betahaus.net>
2018-04-21 16:18:41 +0200

    Not used anylonger

--- voteit/irl/__init__.py
+++ voteit/irl/__init__.py
@@ -15,20 +15,6 @@ def includeme(config):
     config.include('.schemas')
     config.include('.views')
     config.include('.fanstaticlib')
-
     cache_max_age = int(config.registry.settings.get('arche.cache_max_age', 60*60*24))
     config.add_static_view('voteit_irl', 'voteit.irl:static', cache_max_age = cache_max_age)
-
     config.add_translation_dirs('voteit.irl:locale/')
-    #Add translations used within javascript
-    from voteit.core.models.interfaces import IJSUtil
-    js_trans = config.registry.queryUtility(IJSUtil)
-    if js_trans:
-        js_trans.add_translations(
-            register_meeting_presence_error_notice = _(u"register_meeting_presence_error_notice",
-                                                       default = u"VoteIT wasn't able to set you as present. This might be due to server load, "
-                                                       u"please try again in a short while."),
-            presence_success_notice = _(u"Your precence is received"),
-        )
-    else:
-        log.warn("No IJSUtil found so JS translations won't be loaded.")
