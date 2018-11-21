6c7260ebbad7a4122ee27658c2eedcd383d377d6
Johan Schiff <johan.schiff@mp.se>
2018-11-21 14:06:50 +0100

    Gender choice on speaker list join

--- voteit/irl/plugins/gender.py
+++ voteit/irl/plugins/gender.py
@@ -2,18 +2,27 @@
 from __future__ import unicode_literals
 
 from decimal import Decimal
+import json
 
 from arche.interfaces import ISchemaCreatedEvent
 from arche.schemas import SiteSettingsSchema
 from arche.schemas import UserSchema
 import colander
 import deform
+from arche.views.base import BaseForm
+from pyramid.renderers import render
+from pyramid.response import Response
+from pyramid.view import view_config
 
-from voteit.core.models.interfaces import IMeeting
+from voteit.core.models.interfaces import (
+    IMeeting,
+    IUser,
+)
 
 from voteit.irl import _
 from voteit.irl.models.participant_number_ap import ParticipantNumberAP
 from voteit.irl.schemas import ClaimParticipantNumberSchema
+
 
 PRONOUN_VALUES = (('she', _('She')),
                   ('he', _('He')),
@@ -102,12 +111,74 @@ def add_gender_in_meeting(schema, event):
         add_gender_in_schema(schema, event)
 
 
+def add_gender_in_speaker_list(schema, event):
+    schema.description = _('You have to select gender to join a speaker list.')
+    add_gender_in_schema(schema, event)
+
+
+def add_gender_in_speaker_list_settings(schema, event):
+    request = event.request
+    if request.root.site_settings.get('pronoun_active'):
+        title = _('Show gender or pronoun in speaker lists')
+        values = (('', _('No')), ('gender', _('Gender')), ('pronoun', _('Pronoun')))
+    else:
+        title = _('Show gender in speaker lists')
+        values = (('', _('No')), ('gender', _('Yes')))
+    schema.add(colander.SchemaNode(
+        colander.String(),
+        name='show_gender_in_speaker_list',
+        tab=_('Gender'),
+        title=title,
+        widget=deform.widget.RadioChoiceWidget(values=values),
+        default='',
+        missing='',
+    ))
+    schema.add(colander.SchemaNode(
+        colander.Boolean(),
+        name='require_gender_in_speaker_list',
+        tab=_('Gender'),
+        title=_('Require gender to join speaker list'),
+        description=_('Users will have to select gender to be able to join a speaker list.'),
+        default=False,
+        missing=False,
+    ))
+
+
+@view_config(context=IUser, name='gender_settings', renderer='arche:templates/form.pt')
+class GenderSettingsForm(BaseForm):
+    schema_name = 'user_join_speaker_list'
+    type_name = 'SpeakerLists'
+    title = _("Gender settings")
+    formid = 'user_join_speaker_list'
+    use_ajax = True
+
+    def _remove_modal_response(self, appstruct=None, value=None):
+        data = value is not None and {'callback_value': json.dumps(value)} or {}
+        return Response(render("arche:templates/deform/destroy_modal.pt", data, request=self.request))
+
+    cancel_success = cancel_failure = _remove_modal_response
+
+    def save_success(self, appstruct):
+        self.context.update(**appstruct)
+        return self._remove_modal_response(value='resolve')
+
+
 def includeme(config):
     config.add_subscriber(add_gender_in_meeting, [ClaimParticipantNumberSchema, ISchemaCreatedEvent])
     config.add_subscriber(add_gender_in_schema, [UserSchema, ISchemaCreatedEvent])
     config.add_subscriber(pronoun_in_site_settings, [SiteSettingsSchema, ISchemaCreatedEvent])
+    try:
+        from voteit.debate.schemas import (
+            UserJoinSpeakerList,
+            SpeakerListSettingsSchema,
+        )
+        config.add_subscriber(add_gender_in_speaker_list, [UserJoinSpeakerList, ISchemaCreatedEvent])
+        config.add_subscriber(add_gender_in_speaker_list_settings, [SpeakerListSettingsSchema, ISchemaCreatedEvent])
+    except ImportError:
+        pass
     config.registry.registerAdapter(ParticipantNumberAPWithGender, name=ParticipantNumberAPWithGender.name)
     # Add attribute to User class
     from voteit.core.models.user import User
     User.add_field('gender')
     User.add_field('pronoun')
+    config.scan(__name__)
