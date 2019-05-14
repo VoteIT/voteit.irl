9ccb03670fcee8c0a450a69af35df21ac05bb467
Robin Harms Oredsson <robin@betahaus.net>
2019-05-14 16:33:05 +0200

    Static filenames

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -1,8 +1,16 @@
 const path = require("path");
 
+let outputDir = path.resolve(__dirname, "../voteit/irl/static/vue");
+
 module.exports = {
-  outputDir: path.resolve(__dirname, "../voteit/irl/static/vue"),
+  outputDir: outputDir,
   configureWebpack: {
+    output: {
+      filename: "js/bundle.js",
+      chunkFilename: "js/vendor_chunks.js"
+      //filename: outputDir + "hej.js",
+      //chunkFilename: outputDir + "/hej.js"
+    },
     resolve: {
       alias: {
         'src': path.resolve(__dirname, 'src'),
@@ -10,6 +18,18 @@ module.exports = {
         'components': path.resolve(__dirname, 'src/components'),
         'arche': path.resolve(__dirname, 'src/arche')
       }
+    }
+  },
+  chainWebpack: config => {
+    if (config.plugins.has("extract-css")) {
+      const extractCSSPlugin = config.plugin("extract-css");
+      extractCSSPlugin &&
+        extractCSSPlugin.tap(() => [
+          {
+            filename: "css/bundle.css",
+            chunkFilename: "css/vendor_chunks.css"
+          }
+        ]);
     }
   }
 }
